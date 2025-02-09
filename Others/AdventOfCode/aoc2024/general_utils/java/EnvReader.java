c9f049dcb8423fd7c3b46d0bd1c006402e2f782e
Donald Roman <romanunuose@gmail.com>
2025-02-09 14:51:44 +0100

    feat: added a class for reading environment variables

--- Others/AdventOfCode/aoc2024/general_utils/java/EnvReader.java
+++ Others/AdventOfCode/aoc2024/general_utils/java/EnvReader.java
@@ -3,21 +3,39 @@ package Others.AdventOfCode.aoc2024.general_utils.java;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class EnvReader {
-  public static String readEnv(Path path) throws IOException {
-    String content = Files.readString(path);
-    return content;
+  //TODO: Write custom Java code that searches for the .env at the top level of the directory
+  public static List<String> readEnvFile(Path path) throws IOException {
+    return Files.readAllLines(path);
   }
 
-  public static void main(String[] args) {
-    var path = Path.of("Others/AdventOfCode/.env");
-    try {
-      System.out.println(EnvReader.readEnv(path));
-    } catch (IOException e) {
-      System.out.println("Eren Jaeger");
-      // TODO Auto-generated catch block
-      e.printStackTrace();
+  public static Map<String, String> convertLinesToMap(String[] lineStrings) {
+    //Lookup table
+    Map<String, String> envMap = new HashMap<>();
+    for (String line : lineStrings) {
+      String[] lineParts = line.split("=");
+      envMap.put(lineParts[0], lineParts[1]);
     }
+
+    return envMap;
+  }
+
+  public static String readEnv(String env) {
+    var path = Path.of("Others/AdventOfCode/.env");
+    String[] lines;
+    try {
+      lines = EnvReader.readEnvFile(path).toArray(new String[0]);
+    } catch (IOException e) {
+      throw new Error("File Not Found");
+    }
+
+    var envMap = convertLinesToMap(lines);
+
+    // read from the lookup table and return the value
+    return envMap.get(env);
   }
 }
