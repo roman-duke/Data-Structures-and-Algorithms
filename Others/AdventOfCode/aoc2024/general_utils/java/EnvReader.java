1007e9fe7f7ef66e37342a0e522e8bb89b5dbbcf
Donald Roman <romanunuose@gmail.com>
2025-02-09 01:51:59 +0100

    java's classpath and package style will not kill me.

--- Others/AdventOfCode/aoc2024/general_utils/java/EnvReader.java
+++ Others/AdventOfCode/aoc2024/general_utils/java/EnvReader.java
@@ -1,5 +1,23 @@
 package Others.AdventOfCode.aoc2024.general_utils.java;
 
-public class EnvReader {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+public class EnvReader {
+  public static String readEnv(Path path) throws IOException {
+    String content = Files.readString(path);
+    return content;
+  }
+
+  public static void main(String[] args) {
+    var path = Path.of("Others/AdventOfCode/.env");
+    try {
+      System.out.println(EnvReader.readEnv(path));
+    } catch (IOException e) {
+      System.out.println("Eren Jaeger");
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
 }
