7536bd45729c41663d5f9987a1932ffed9b91cab
Donald Roman <romanunuose@gmail.com>
2025-02-09 16:40:49 +0100

    solved the day01 part 1 of AOC2024 using the naive solution
    This current approach involves about 3 full passes of the input array. The ideal solution would be to do the scanning and parsing in one pass. If time permits, we shall come to this

--- Others/AdventOfCode/aoc2024/day01/javaAnswers/Day01.java
+++ Others/AdventOfCode/aoc2024/day01/javaAnswers/Day01.java
@@ -1,16 +1,51 @@
 package Others.AdventOfCode.aoc2024.day01.javaAnswers;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import Others.AdventOfCode.aoc2024.day01.javaAnswers.utils.java.Questionfetcher;
 
 public class Day01 {
   public static void main(String[] args) {
-    String input;
+    String input = "";
+    List<Integer> column1 = new ArrayList<Integer>();
+    List<Integer> column2 = new ArrayList<Integer>();
+    int sumOfDifference = 0;
+
     try {
       input = Questionfetcher.fetchQuestion();
-      System.out.println(input);
+      // System.out.println(input);
     } catch (IOException | InterruptedException e) {
         e.printStackTrace();
     }
+
+    // Parse the input and store the items respectively in the correct destinations
+    // This here is quite interesting; is there any way for us to do this in one pass?
+    // By using the built-in split method to split the string by multiple lines, internally
+    // java has to go through the whole string
+    // We should do this in one pass by manually scanning the string and then identifying if we've
+    // encountered a new line break or a white space (indicating the start of a new "lexeme")
+    // But I am too lazy for that actually
+
+    // A n-element array of strings from each line of the input
+    String[] inputArr = input.split("\n");
+
+    for (String line : inputArr) {
+      String[] lineArr = line.split("\\s+");
+      column1.add(Integer.valueOf(lineArr[0]));
+      column2.add(Integer.valueOf(lineArr[1]));
+    }
+
+    // Now sort both lists
+    Collections.sort(column1, (a, b) -> a.compareTo(b));
+    Collections.sort(column2, (a, b) -> a.compareTo(b));
+
+    for (int i = 0; i < column1.size(); i++) {
+      sumOfDifference += Math.abs(column1.get(i) - column2.get(i));
+    }
+
+    System.out.println("The sum of the differences is --> " + sumOfDifference);
   }
 }
--- Others/AdventOfCode/aoc2024/day01/javaAnswers/utils/java/Questionfetcher.java
+++ Others/AdventOfCode/aoc2024/day01/javaAnswers/utils/java/Questionfetcher.java
@@ -6,27 +6,34 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import Others.AdventOfCode.aoc2024.general_utils.java.EnvReader;
+
 public class Questionfetcher {
-  // TODO: Write a custom class that retrieves the environment variable for the relevant day from the .env file
-  private static final String URL_STRING = System.getenv("ADVENT_OF_CODE_DAY_1_INPUT_URL");
+  // TODO: Write a custom class that retrieves the environment variable for the
+  // relevant day from the .env file
+  private static final String URL_STRING = EnvReader.readEnv("ADVENT_OF_CODE_DAY_1_INPUT_URL");
 
   public static String fetchQuestion() throws IOException, InterruptedException {
     System.out.println("Here is the environment variable --> " + URL_STRING);
     // Create an HttpClient instance
     HttpClient client = HttpClient.newHttpClient();
 
-    // Create a request
+    //TODO: Automate the process of getting the cookies (essentially the login flow)
+    // Define cookies
+    String cookies = "_ga=GA1.2.950787434.1738016527;" +
+      "_gid=GA1.2.368833985.1739110144;" +
+      "session=53616c7465645f5fbc0fff6561263ea20d4a49de9498927af0af0f76a43670549f50e8da8d44365f25fd86360e4d0521f41c453a13b26748e7c1fb3200452dda;" +
+      "_ga_MHSNPJKWC7=GS1.2.1739110145.2.1.1739110671.0.0.0";
+
+    // Create a request with cookies in the header
     HttpRequest request = HttpRequest.newBuilder()
         .uri(URI.create(URL_STRING))
+        .header("Cookie", cookies)
         .GET()
         .build();
 
     // Send the request and get the response
     HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-    // Print the status code and response body
-    System.out.println("Status Code: " + response.statusCode());
-    System.out.println("Response Body: " + response.body());
 
     return response.body();
   }
